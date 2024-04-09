fcafe77d3fd6d84c5071cf45e7f04644b7235e4a
fvaldes0109 <fvaldes0109@gmail.com>
2024-04-09 09:16:08 +0200

    Update comments

--- transactions.py
+++ transactions.py
@@ -13,8 +13,7 @@ from ipv8_service import IPv8
 
 @dataclass(msg_id=1)  # The value 1 identifies this message and must be unique per community
 class MyMessage:
-    amount: int  # We add an integer (technically a "long long") field "clock" to this message
-
+    amount: int
 
 class MyCommunity(Community):
     community_id = os.urandom(20)
@@ -23,27 +22,21 @@ class MyCommunity(Community):
         super().__init__(settings)
         # Register the message handler for messages (with the identifier "1").
         self.add_message_handler(MyMessage, self.on_message)
-        # The Lamport clock this peer maintains.
-        # This is for the example of global clock synchronization.
         self.balance = 100
 
     def started(self) -> None:
         async def start_communication() -> None:
-            # If we have not started counting, try boostrapping
-            # communication with our other known peers.
             for p in self.get_peers():
                 to_send = random.randint(0, self.balance)
                 self.balance -= to_send
                 self.ez_send(p, MyMessage(to_send))
 
         # We register an asyncio task with this overlay.
-        # This makes sure that the task ends when this overlay is unloaded.
         # We call the 'start_communication' function every 5.0 seconds, starting now.
         self.register_task("start_communication", start_communication, interval=5.0, delay=0)
 
     @lazy_wrapper(MyMessage)
     def on_message(self, peer: Peer, payload: MyMessage) -> None:
-        # Update our Lamport clock.
         self.balance += payload.amount
         print(self.my_peer, "sent", payload.amount, "to", peer, " - Current balance:", self.balance)
 
